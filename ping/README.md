345d32b522adc57b1d3f24211a95f825ec9276bb
Mark H <markhawkins@blueyonder.co.uk>
2024-12-08 18:47:48 +0000

    Basic None K8's Feign Client Implemented

---
+++ ping/README.md
@@ -0,0 +1,3 @@
+## Using Feign and Kubernetes
+Need to create a service account etc see:
+* https://medium.com/@iK8sJourneyMaverick/efficient-communication-in-spring-boot-applications-leveraging-feign-client-on-kubernetes-41f4c78b01c3
--- ping/src/main/java/com/motomark/ping/PongClient.java
+++ ping/src/main/java/com/motomark/ping/PongClient.java
@@ -3,8 +3,8 @@ package com.motomark.ping;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
-@FeignClient(name = "pong-service")
+@FeignClient(name = "pong-service", url = "http://pong-service:9900", path = "/")
+//@FeignClient(name = "pong-service")
 public interface PongClient {
     @GetMapping("/pong")
     public String sayPong();
